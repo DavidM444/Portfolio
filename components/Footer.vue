be2cdacd33d9730a922fbdde19fd94f10e58c793
DavidM444 <david541gamer@gmail.com>
2024-03-06 19:57:20 -0500

    Footer component for layout

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-12 col-sm-8 m-auto">
+    <div class="card">
+      <div class="card-body">
+          <footer>
+            <p align="center" class="fs">© <!-- -->{{ time }}<!-- --> David Muñoz. All Rights Reserved.</p>
+          </footer>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script lang="ts" setup>
+const time = new Date().getFullYear()
+</script>
+
+<style scoped>
+.fs{
+  font-size: 14px;
+  align-self: center;
+}
+
+</style>
