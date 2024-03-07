6137f6d12ae3e2960df093ec4e8d341c8bb15e42
DavidM444 <david541gamer@gmail.com>
2024-03-06 19:58:18 -0500

    Principal description component for layout

---
+++ components/Vista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="fondo img-fluid container text-center">
+      <h2>Rodrigo David Muñoz</h2>
+      <h3>Desarrollador Web</h3>
+
+    </div>
+    
+  </div>
+</template>
+<style scoped>
+.fondo {
+  background-color: rgb(116, 82, 82);
+  margin-top: 1rem;
+
+  min-height: 30vh;
+  background-size: cover;
+
+}
+
+h2 {
+  font-family: 'Open sans', sans-serif;
+
+  font-weight: 600;
+  padding-top: 2rem;
+}
+
+h3 {
+  color: brown;
+  font-family: "Nunito", sans-serif;
+  font-weight: 700;
+
+}
+</style>
