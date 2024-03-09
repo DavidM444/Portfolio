67ea366b510981e797469fad14206826920c0ca4
DavidM444 <david541gamer@gmail.com>
2024-03-08 20:15:37 -0500

    Delete transition style

--- app.vue
+++ app.vue
@@ -3,5 +3,18 @@
   <NuxtLayout>
     <NuxtPage/>
   </NuxtLayout>
-
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+  background-color: rgb(62, 223, 22);
+}
+.page-enter-from,
+.page-leave-to {
+  background-color: rgb(55, 31, 162);
+  opacity: 2;
+  filter: blur(1rem);
+}
+
+</style>
