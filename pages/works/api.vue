b48893f5d39b6bba0574723519a7e158e746da02
DavidM444 <david541gamer@gmail.com>
2024-03-08 20:16:57 -0500

    route /api transition

---
+++ pages/works/api.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Navar />
+    <div>
+      <h2>Desde pagina API</h2>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: false,
+})
+
+</script>
+
+<!--Transition for pages on works/   -->
+<style>
+.slide-enter-active, .slide-leave-active {
+  transition: transform 0.5s ease-in-out;
+}
+
+.slide-enter-from, .slide-leave-to {
+  transform: translateX(100%);
+}
+</style>
+
