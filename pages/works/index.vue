40f8fcd185715f049c3842cb2dcbf3143fc18f02
DavidM444 <david541gamer@gmail.com>
2024-03-09 20:33:45 -0500

    card padding

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -2,20 +2,20 @@
     <div class="container">
       <h3>Works</h3>
       <section class="row">
-        <a href="/works/api" class="pt-md-0 pt-3 col-12 col-sm-4 ">
+        <a href="/works/api" class=" pt-3 col-12  col-sm-6">
           <div class="card tarjeta" >
-            <img src="~/assets/api.jpg" class="card-img-top " alt="...">
-            <div hre class="card-body">
+            <img src="~/assets/api.jpg" class=" " alt="...">
+            <div>
               <h5  class="card-title">Api </h5>
             </div>
             <p class="card-text">Api destinada al manejo y gestion de registros de calidad textil.
                 Dispone de Login y gestion de roles para los usuarios. </p>
             </div>
         </a>
-        <a href="/works/quality" class="pt-3 pt-sm-0 col-12 col-sm-4 ">
+        <a href="/works/quality" class="pt-3  col-12 col-sm-6 ">
           <div class="card tarjeta">
-            <img src="~/assets/qualitytex.jpg" class="card-img-top " alt="...">
-            <div class="card-body">
+            <img src="~/assets/qualitytex.jpg" alt="...">
+            <div>
               <h5 class="card-title">Quality Tex</h5>
             
             </div>
@@ -24,10 +24,10 @@
                 </p>
           </div>
         </a>
-        <a href="/works/pokedex" class="pt-3 pt-sm-0 col-12 col-sm-4 ">
+        <a href="/works/pokedex" class="pt-3  col-12 col-sm-6">
           <div class="card tarjeta">
-            <img src="~/assets/oip.jpg" class="card-img-top " alt="...">
-            <div class="card-body">
+            <img src="~/assets/oip.jpg" alt="...">
+            <div>
               <h5 class="card-title">Pokedex</h5>
              
               
@@ -43,16 +43,34 @@
 
 <style scoped>
 
+
+
 .tarjeta > div > h5{
-  color: #0f9faf;
+  color: white;
+  font-size: 20px;
+  margin-top: 0.6rem;
+  text-align: center;
 }
 
 .tarjeta > p{
   color: rgb(0, 0, 0);
+  text-align: center;
+  word-wrap: break-word;
+  font-size: 14px;
+  color: white;
+}
+.card{
+  background-color: transparent;
 }
 
 a{
   text-decoration: none;
+}
+
+.row{
+  row-gap: 1.5rem;
+  grid-template-columns: repeat(1, minmax(0px, 1fr));
+
 }
 
 
