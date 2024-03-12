de9c2d218795f8ef221da389de991a8ef078d1d3
DavidM444 <david541gamer@gmail.com>
2024-03-11 19:12:05 -0500

    new layout without vista

---
+++ layouts/subroutes.vue
@@ -0,0 +1,22 @@
+<template>
+  <div> 
+      <div class="page2">
+        <Navar/>
+      <slot></slot>
+
+      <Footer/>
+      </div>
+  </div>
+</template>
+
+
+<style>
+.page2{
+  max-width: 60ch;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  padding-inline-start: 1rem;
+  padding-inline-end: 1rem;
+
+}
+</style>
