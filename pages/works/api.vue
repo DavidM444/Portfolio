8f63d676994e68a8e08e498e0c7b53574ec6ec4b
DavidM444 <david541gamer@gmail.com>
2024-03-11 19:13:43 -0500

    structure api page with technologies

--- pages/works/api.vue
+++ pages/works/api.vue
@@ -1,8 +1,89 @@
 <template>
-  <div>
-    <Navar />
+  <div class="row">
+    <div class="col text-center">
+      <h2>API Quality Tex</h2>
+      <p>Quality Tex, es una API de una empresa textil, que como plataforma que centraliza y administra datos
+        relacionados con muestras textiles y pruebas de calidad.
+        Permite el registro y autenticación de usuarios para acceder a la plataforma, garantizando la seguridad de la
+        información.
+      </p>
+      <p>
+        Los usuarios pueden cargar datos de muestras textiles, incluyendo información sobre pruebas realizadas y sus
+        resultados.
+        La API ofrece funcionalidades para visualizar, buscar y analizar datos de muestras, así como generar informes
+        sobre la calidad de las telas.
+
+      </p>
+      <p>
+        Además, proporciona herramientas para la gestión de usuarios, roles y permisos, asegurando un control preciso
+        sobre quién puede acceder y modificar la información.
+      </p>
+
+    </div>
+
     <div>
-      <h2>Desde pagina API</h2>
+      <div>
+        <h3>Tecnologias y recursos</h3>
+      </div>
+      <div class="table-responsive-xl">
+        <table class="table table-primary">
+          <tbody>
+            <tr class="">
+              <td scope="row">
+                <Icon name="vscode-icons:file-type-java" />
+                <span class="iconify" data-icon="vscode-icons:file-type-java" data-inline="false"></span>
+
+              </td>
+              <td>Java</td>
+              <td>Lenguaje de Programacion</td>
+            </tr>
+            <tr class="">
+              <td scope="row">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 48 48">
+                  <path fill="#8bc34a"
+                    d="M43.982,23.635c0.069-4.261-0.891-9.328-2.891-15.273l-1.568-4.662l-2.13,4.433 c-0.114,0.237-0.244,0.469-0.38,0.698C33.514,5.827,28.974,4,24,4C12.954,4,4,12.954,4,24c0,11.046,8.954,20,20,20s20-8.954,20-20 C44,23.877,43.984,23.758,43.982,23.635z">
+                  </path>
+                  <path fill="#fff"
+                    d="M39.385 32.558c-3.123 4.302-8.651 4.533-13.854 4.442H18.75h-1.938c4.428-1.593 7.063-1.972 9.754-3.4 5.068-2.665 10.078-8.496 11.121-14.562-1.93 5.836-7.779 10.85-13.109 12.889-3.652 1.393-10.248 2.745-10.248 2.745l-.267-.145C9.573 32.268 9.437 22.214 17.6 18.968c3.574-1.423 6.993-.641 10.854-1.593 4.122-1.012 8.89-4.208 10.83-8.375C41.456 15.667 44.07 26.106 39.385 32.558L39.385 32.558zM15.668 38.445C15.386 38.795 14.955 39 14.505 39c-.823 0-1.495-.677-1.495-1.5s.677-1.5 1.495-1.5c.341 0 .677.118.941.336C16.086 36.855 16.186 37.805 15.668 38.445L15.668 38.445z">
+                  </path>
+                </svg>
+              </td>
+              <td>Spring Boot</td>
+              <td>Framework de desarrollo</td>
+            </tr>
+            <tr class="">
+              <td scope="row">
+                <Icon name="vscode-icons:file-type-mysql" />
+              </td>
+              <td>MySql</td>
+              <td>Base de datos</td>
+            </tr>
+            <tr class="">
+              <td scope="row">
+                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" height="40" width="40">
+                  <path
+                    d="m3.02 21.055 1.12-.23c1.054-.219 2.114-.415 3.161-.66a4.955 4.952 0 0 0 3.696-3.915 6.06 6.057 0 0 0 .085-1.153l.366-.09a4.936 4.933 0 0 0 3.75-3.717 3.8 3.798 0 0 0 .128-1.06c-.002-.096 0-.193 0-.305.094-.023.18-.047.267-.067a4.552 4.549 0 0 0 2.68-1.755 5.772 5.769 0 0 0 1.005-4.854c-.021-.088-.035-.179-.061-.264a.63.629 0 0 0-.728-.432l-3.184.652-3.485.716-4.002.822q-2.186.449-4.373.893a1.125 1.125 0 0 1-.422.06c-.007-.074-.017-.14-.017-.204-.001-1.12.002-2.243-.005-3.364a.324.324 0 0 1 .174-.307 8.493 8.488 0 0 1 1.9-.86A17.205 17.194 0 0 1 7.827.315C8.373.23 8.922.181 9.47.12a26.795 26.777 0 0 1 3.526-.102c.496.01.992.051 1.487.097.542.051 1.085.11 1.623.192a14.482 14.472 0 0 1 4.007 1.124c.262.123.509.28.764.422a.215.215 0 0 1 .122.223c-.004.054 0 .11 0 .163v19.519c0 .347.045.28-.262.472a8.437 8.432 0 0 1-1.961.857 16.78 16.769 0 0 1-2.851.63c-.6.08-1.2.146-1.804.207-.277.03-.556.035-.835.043-.564.015-1.128.041-1.691.03-.636-.014-1.272-.059-1.907-.099a20.054 20.041 0 0 1-2.519-.332 13.423 13.415 0 0 1-3.224-.976c-.273-.13-.53-.29-.797-.435a.246.246 0 0 1-.144-.248c.008-.23 0-.46.003-.69 0-.049.01-.096.016-.163zm9.985-10.652a1.248 1.248 0 0 1-.09.55 2.624 2.622 0 0 1-2.045 1.84c-1.3.28-2.607.537-3.912.805l-.57.114a1.147 1.146 0 1 0 .403 2.256c.595-.11 1.186-.242 1.779-.363.064-.014.128-.022.209-.035a2.584 2.582 0 0 1-.55 1.41A2.801 2.8 0 0 1 6.516 18c-1.103.223-2.206.45-3.308.676-.052.01-.106.013-.156.02-.048-.146-.061-10.38-.014-10.63l14.048-2.883a1.523 1.522 0 0 1-.016.18 3.438 3.436 0 0 1-.738 1.502 2.399 2.397 0 0 1-1.426.804c-1.11.216-2.218.45-3.326.677l-.96.196a1.168 1.168 0 0 0-.953 1.057 1.147 1.147 0 0 0 .875 1.186 1.747 1.746 0 0 0 .807-.054c.492-.1.983-.203 1.475-.304.053-.01.108-.014.18-.023z" />
+                </svg>
+
+              </td>
+              <td>FlyWay</td>
+              <td>Herramienta de migracion de base de datos</td>
+            </tr>
+            <tr class="">
+              <td scope="row">
+                
+              </td>
+              <td>Jwt Token</td>
+              <td>Crador de tokens de seguridad que perminte propagacion de privilegios(claims) e identidad</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
+
+
+
     </div>
   </div>
 
@@ -10,7 +91,7 @@
 
 <script lang="ts" setup>
 definePageMeta({
-  layout: false,
+  layout: 'subroutes',
 })
 
 </script>
