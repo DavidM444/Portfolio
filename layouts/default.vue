d9c33e5ef4d7a5e8b9f20b3c07da3cbecf75f6dc
DavidM444 <david541gamer@gmail.com>
2024-03-12 21:21:06 -0500

    layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,25 @@
 <template>
-    <div class="container-fluid">
-        <Navar/>
-        <Vista/>
-        <slot></slot>
-        <Footer/>
+    <div>
         
+        <div class="page2" >
+          <Navar/>
+          <Vista/>
+        <slot></slot>
+
+        <Footer/>
+        </div>
     </div>
 </template>
+
+<style>
+
+.page2{
+  max-width: 60ch;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  padding-inline-start: 1rem;
+  padding-inline-end: 1rem;
+
+}
+
+</style>
