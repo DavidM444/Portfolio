011ed5d893354d88e76a54a19d2abb91e1de7523
DavidM444 <david541gamer@gmail.com>
2024-03-12 21:24:19 -0500

    new github svg and sytles layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-     <div>
+    <div>
       <div class="container description">
         <div class="text-center p-3 p-sm-1">Hi! i am Backend Developer from Colombia.</div>
 
       </div>
-      <div class="container-flui d-flex">
-        <div class="text-left">
+      <div class="row">
+        <div class="text-left col-md-8 ">
           <h2>David Muñoz</h2>
           <p>Amante Tecnologico(Developer /Deportist /Gamer)</p>
         </div>
-        <div class="text-center align-self-center">
-          <img loading="lazy" src="../assets/mylogo.jpg" alt="" class="image-fluid card-img-top face">
+        <div class="text-center align-self-center col-md-4">
+          <img loading="lazy" src="../assets/mylogo.jpg" alt="" class=" card-img-top face">
         </div>
       </div>
     </div>
@@ -59,14 +59,8 @@
           <li>
             <a target="_blank" href="https://github.com/DavidM444">
               <button>
-                <span>
-                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" aria-hidden="true"
-                    focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z">
-                    </path>
-                  </svg>
-                  @davidm444</span>
+                <Icon name="bxl:github" size="1.5em" />
+                @davidm444
               </button>
             </a>
           </li>
@@ -74,21 +68,12 @@
       </div>
     </article>
   </div>
-   
+
 
 </template>
 
 
 <style scoped>
-.fondo {
-  background-color: rgb(116, 82, 82);
-  margin-top: 1rem;
-
-  min-height: 30vh;
-  background-size: cover;
-
-}
-
 h2 {
   font-family: 'Open sans', sans-serif;
 
@@ -103,12 +88,13 @@ h3 {
 
 }
 h3,h2,span,p,div{
-    color: rgb(252, 245, 245);
+    color: ''
 }
 
 .face {
+  position: relative;
   height: auto;
-  width: 11vh;
+  width: 16vh;
   padding: 8px;
   border-radius: 50%;
 }
@@ -131,7 +117,6 @@ h3,h2,span,p,div{
 
  .description{
   border-radius: 1rem;
-  background-color: gray;
   padding: 10px;
  }
 
