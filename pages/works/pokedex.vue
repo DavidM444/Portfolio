a469941e62da8ec342006885f50d736f9bfba537
DavidM444 <david541gamer@gmail.com>
2024-03-13 22:40:01 -0500

    pokedex info

---
+++ pages/works/pokedex.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row">
+
+    <div class="col text-center">
+      <h2>Pokedex</h2>
+
+
+
+    </div>
+    <div class="row p-3">
+      <img src="~/assets/oip.jpg" alt="">
+    </div>
+    <div class="tex-center">
+      <p>Es una web que muestra toda la informacion acerca de los pokemones que existen en la api PokeApi. Esta web
+        consume esta api para mostrar de
+        manera dinamica la informacion y datos que esta api provee.
+      </p>
+    </div>
+    <div>
+      <div>
+        <h3>Tecnologias y recursos</h3>
+      </div>
+      <div class="table-responsive-xl">
+        <table class="table table-primary">
+          <tbody>
+            <tr class="">
+              <td scope="row">
+                <Icon name="devicon:javascript" size="2em" />
+              </td>
+              <td>JavaScript</td>
+              <td>Lenguaje de Programacion</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
+
+
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
