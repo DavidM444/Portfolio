966fc26156fef13c143044ae7d803485f69025f9
DavidM444 <david541gamer@gmail.com>
2024-11-16 18:58:56 -0500

    Send email working

--- components/Form.vue
+++ components/Form.vue
@@ -3,47 +3,88 @@
     <form id="sendMessage" method="post" @submit.prevent="handleSubmit">
       <div class="mb-3">
         <label for="name" class="form-label">Nombre</label>
-        <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId" v-model="nombre" />
+        <input v-model="emailToSend.nombre" type="text" class="form-control" name="name" id="name" aria-describedby="helpId" />
         <small id="helpId" class="form-text text-muted">Escriba su nombre</small>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Correo Electrónico</label>
-        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
+        <input v-model="emailToSend.email" type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
          @blur="emailTouched = true"
-          placeholder="abc@mail.com" v-model="email"/>
+          placeholder="abc@mail.com" />
         <small id="emailHelpId" class="form-text text-muted" :class="{invalid:!isEmailValid && emailTouched}">
           {{!isEmailValid && emailTouched? ' El correo no parece ser válido':'Introduzca su correo electronico '}}
         </small>
       </div>
       <div class="mb-3">
         <label for="message" class="form-label">Mensaje</label>
-        <input type="text" class="form-control" name="message" id="message" aria-describedby="helpId" placeholder="" v-model="message"/>
+        <input v-model="emailToSend.mensage" type="text" class="form-control" name="message" id="message" aria-describedby="helpId" placeholder="" />
         <small id="helpId" class="form-text text-muted">Registra el mensaje</small>
       </div>
-      <ButtonLink :url=dataButton />
+      <button class="btn btn-success" type="submit">Enviar</button>
     </form>
+    <p v-if="success">¡Mensaje enviado con éxito!</p>
+    <p v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import ButtonLink from './ButtonLink.vue';
 
-const nombre = ref<string>('');
-const email = ref<string>('');
-const message = ref<string>('');
+import { ref } from 'vue';
+
+
+
+//import ButtonLink from './ButtonLink.vue';
+import type { Post } from '~/types/types';
+import { useCreatemail } from '#imports';
+
 const emailTouched = ref<boolean>(false);
+//sende api
+const emailToSend = ref<Post>({
+  nombre: '',
+  email: '',
+  mensage: ''
+})
 
-const handleSubmit = () => {
+const success = ref(false);
+const error = ref<string | null>(null);
+
+const handleSubmit = async () => {
+  console.log("enviando handlesumbmit")
   emailTouched.value = true;
   if (isEmailValid.value) {
+
+    console.log("eviando peticion")
+    try {
+        // Llama a la API con el formulario, de manera directa
+        /*
+      rgb(15, 57, 25)(57, 15, 15)(117, 22, 22)(79, 27, 27)wait $fetch('/api/apiemail', {
+          method: 'POST',
+          body: emialToSend,
+        });
+        */
+
+        //Usando Api a traves de un composable
+        const data = useCreatemail(emailToSend.value);
+
+        success.value = true;
+        error.value = null;
+      } catch (err: any) {
+        success.value = false;
+        error.value = err.message|| 'Error al enviar el correo.';
+      }
+  
+
+
     alert('Formulario enviado con éxito');
+
   } else {
     alert('Por favor, corrige los errores antes de enviar.');
   }
 };
 
+
 const isEmailValid = computed(() => {
-  return isValidEmail(email.value);
+  return isValidEmail(emailToSend.value.email);
 });
 
  function isValidEmail( val:string ):boolean {
@@ -55,6 +96,12 @@ const dataButton = {
   link: "#",
   text: "Send"
 }
+
+
+
+
+
+
 </script>
 
 <style scoped>
@@ -72,7 +119,15 @@ form{
 }
 form input{
   border-color: var(--300);
+  
 }
+
+.form-control:focus{
+  box-shadow: 0 0 8px var(--500) ;
+  border: var(--500);
+
+}
+
 form label{
   font-family: 'Nunito',sans-serif;
 }
